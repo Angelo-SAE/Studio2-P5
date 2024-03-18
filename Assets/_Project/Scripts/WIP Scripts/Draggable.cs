1834f3a04454b0251ddf2a0953dabf656c1d166a
Angelo <a.allaham@student.saedubai.com>
2024-03-18 10:59:57 +0400

    Added player location to tablet display

--- Assets/_Project/Scripts/WIP Scripts/Draggable.cs
+++ Assets/_Project/Scripts/WIP Scripts/Draggable.cs
@@ -7,14 +7,35 @@ public class Draggable : MonoBehaviour
     public bool canDrag;
 
     [SerializeField] private Room room;
+    [SerializeField] private GameObject playerIndicator;
+    private bool showingPlayer;
 
     void Update()
+    {
+      CheckIfCanDrag();
+      ShowPlayerInRoom();
+    }
+
+    private void CheckIfCanDrag()
     {
       if(room.hasPlayer)
       {
         canDrag = false;
       } else {
         canDrag = true;
+      }
+    }
+
+    private void ShowPlayerInRoom()
+    {
+      if(room.hasPlayer && !showingPlayer)
+      {
+        showingPlayer = true;
+        playerIndicator.SetActive(true);
+      } else if(!room.hasPlayer && showingPlayer)
+      {
+        showingPlayer = false;
+        playerIndicator.SetActive(false);
       }
     }
 }
